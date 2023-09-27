715e553ff7f69a620cec1f8c58bf67de5927b25d
Mustafa SANSAR <mustafasansar44@gmail.com>
2023-09-27 19:31:54 +0300

    CustomException yazıldı.

---
+++ src/main/java/com/msansar/wordmemorization/exception/CustomNotFoundException.java
@@ -0,0 +1,11 @@
+package com.msansar.wordmemorization.exception;
+
+public class CustomNotFoundException extends RuntimeException{
+    public CustomNotFoundException(String message) {
+        super(message);
+    }
+
+    public CustomNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
